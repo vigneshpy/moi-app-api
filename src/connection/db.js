09ad39b98c118c42571c65ecc8f03ef3c7706b23
vigneshpy <vickypotter2516@gmail.com>
2025-02-28 00:04:40 +0530

    read me updation

--- src/connection/db.js
+++ src/connection/db.js
@@ -7,11 +7,9 @@ const dbUsername = encodeURIComponent(process.env.DATABASE_USER_NAME);
 const dbNAme = encodeURIComponent(process.env.DATABASE_NAME);
 
 const dbPassword = encodeURIComponent(process.env.DATABASE_PASSWORD);
-console.log("🚀 ~ dbPassword:", dbPassword);
 
 const dbURI = `mongodb+srv://${dbUsername}:${dbPassword}@${dbNAme}.njhcz.mongodb.net/?retryWrites=true&w=majority&appName=moi-app`;
 
-console.log("🚀 ~ dbURI:", dbURI);
 const clientOptions = {
 	serverApi: { version: "1", strict: true, deprecationErrors: true },
 };
