2ffe95670967622fc69ccff619dacf9850be29b8
Vignesh <40609760+vigneshpy@users.noreply.github.com>
2025-03-20 00:28:33 +0530

    Update src/middleware/validateRequest.middleware.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/middleware/validateRequest.middleware.ts
+++ src/middleware/validateRequest.middleware.ts
@@ -26,16 +26,16 @@ export const validateVerifyOTP = (req, res, next: NextFunction) => {
 		return res.status(400).json({ error: "Request body is missing or empty!" });
 	}
 	let { phone_number } = req.body;
-
 	const { otp } = req.body;
-	if (!phone_number.startsWith("+")) {
-		req.body.phone_number = COUNTRY_CODE + phone_number;
-	}
 
 	if (!phone_number || !otp) {
 		return res
 			.status(400)
 			.json({ error: "Phone number and OTP are required!" });
+	}
+
+	if (!phone_number.startsWith("+")) {
+		req.body.phone_number = COUNTRY_CODE + phone_number;
 	}
 
 	next();
